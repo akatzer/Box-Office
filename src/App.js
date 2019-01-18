a121d4768fed47816380083754b3beedf8b1fd4e
Addison Murry <addymurry@gmail.com>
2019-01-17 18:26:05 -0600

    merge

--- src/App.js
+++ src/App.js
@@ -7,43 +7,15 @@ import Search from "./components/searchBox/search";
 import "./App.css";
 
 
-
 class App extends Component {
     
-    // Setting this.state.matches to the movies json array
-    state = {
-        matches,
-
-    };
-
-
-
-        // Otherwise, if clicked = false, and the user hasn't finished
-
     render() {
         return (
             <Wrapper>
-                
-                <Title> Blockbuster your new favorite site </Title>
+                <Title>Box Office</Title>
         
-
-                
-                <div className="container">
-                <div className="row">
-                
-                
-                {this.state.matches.map(match => (
-                    <MovieCard
-                        setClicked={this.setClicked}
-                        id={match.id}
-                        key={match.id}
-                        image={match.image}
-                    />
-                ))}
-                
-                </div>
-                </div>
-                <Search> Search a movie here</Search>
+            <MovieCard></MovieCard>
+                    
             </Wrapper>
             
             
